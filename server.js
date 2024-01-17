2c645d104a57630eab360f7226fed92ab1f1bac3
Gakpe <47566265+Gakpe@users.noreply.github.com>
2024-01-17 22:10:23 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const { forceDomain } = require('forcedomain');
+
+
+
+// Init the Parseur Middleware //
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+// Serve static assets for production //
+
+if(process.env.NODE_ENV === 'production') {
+
+  app.use(forceDomain({
+    hostname: 'www.sports-talents-management.com',
+    protocol: 'https'
+  }));
+
+  // Alors il faut set un static folder //
+  app.use(
+    express.static('client/build')
+  );
+
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build','index.html'));
+  });
+}
+
+// Setting du port //
+const port = process.env.PORT || 8000;
+app.listen(port,() => console.log(`Serveur starting on ${port}`));
